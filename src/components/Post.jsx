5e9cf6b7f11eb35414afbb52f48021a565e0a317
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-07-27 13:20:46 -0700

    Adds read, update, and delete post queries

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ const Post = (props) => {
     <div>
         <div className="posts-display">
         <h4>{props.title}</h4>
-        <span className="post-poster">{props.poster}</span>
+        <span className="post-poster">by {props.poster}</span>
         <span className="post-text">{props.body}</span>
       </div>
     </div>
--- src/queries/PostQuery.jsx
+++ src/queries/PostQuery.jsx
@@ -6,7 +6,43 @@ class PostQuery {
     .then(response => response.json())
     .then(jsonData => jsonData.posts)
     .catch(err => console.log(err))
-  }
-}
+  };
+
+  static show = (id) => {
+    return fetch(`${url}/${id}`)
+    .then(response => response.json())
+    .then(jsonData => jsonData.game)
+    .catch(err => console.log(err))
+  };
+
+  static create = (postData) => {
+    return fetch (`${url}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+  };
+
+  static update = (id, gameData) => {
+    return fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(gameData)
+    })
+    .then(response => response.json())
+  };
+
+  static delete = (id) => {
+    return fetch(`${url}/${id}`, {
+      method: "DELETE"
+    })
+    .then(response => response.json())
+  };
+};
 
 export default PostQuery
