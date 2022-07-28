6a04e63140e708533b2431c0f45362b3b6bd8f27
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-07-28 15:31:54 -0700

    Adds post delete functionality

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import CitiesContainer from "../components/CitiesContainer";
 import PostQuery from "../queries/PostQuery";
 
@@ -7,8 +7,7 @@ const PostPage = () => {
   const [post, setPost] = useState({})
 
   let id = useParams().id;
- 
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
 
   useEffect(() => {
     PostQuery.show(id).then(data => setPost(data))
@@ -22,6 +21,9 @@ const PostPage = () => {
       <h3>by {post.poster}</h3>
       <p>{post.body}</p>
       <Link to={`/cities/san-francisco/${id}/edit`}>Edit Post</Link>
+      <button onClick={() => PostQuery.delete(id).then(data => {navigate("/cities/san-francisco")})}>
+        Delete Post
+      </button>
     </div>
     </div>  
   )
